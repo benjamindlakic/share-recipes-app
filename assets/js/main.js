8609a8b79cd7f4234615dc4a18615c33b7e1b330
benjamindlakic <benjomaster7@gmail.com>
2024-03-31 22:01:04 +0200

    Added ajax calls

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,19 +35,27 @@
   });
 })(jQuery);
 
-
 $(document).ready(function () {
+  $(document).on("click", ".btn-like", function () {
+    $(this).toggleClass("liked");
+    var icon = $(this).find("i");
+    icon.toggleClass("bi-heart bi-heart-fill");
+  });
+
   // Function to fetch and display recipe data
   function getRecipes() {
     $.get("recipes.json", function (data) {
       data.forEach(function (recipe) {
+        var limitedDescription =
+          recipe.description.slice(0, 150) +
+          (recipe.description.length > 200 ? "..." : "");
         var recipefeedHtml = `
                         <div class="card">
                             <a href=""><img src="${recipe.image}" class="card-img-top" alt="${recipe.title}">
                             <div class="card-body">
                                 <h5 class="card-title">${recipe.title}</h5>
                             </a>
-                                <p class="card-text">${recipe.description}</p>
+                                <p class="card-text">${limitedDescription}</p>
                                 <div class="social-buttons">
                                     <button class="btn btn-like"><i class="bi bi-heart"></i> Like</button>
                                     <button class="btn btn-comment"><i class="bi bi-chat"></i> Comment</button>
@@ -56,7 +64,7 @@ $(document).ready(function () {
                             </div>
                         </div>
                         `;
-        $("#feed").append(recipefeedHtml);
+        $("#feedContainer").append(recipefeedHtml);
       });
     });
   }
@@ -112,7 +120,64 @@ $(document).ready(function () {
       });
     });
   }
+
+  function getFeaturedRecipes() {
+    $.get("recipes.json", function (data) {
+      data.sort(() => Math.random() - 0.5);
+      data.slice(0, 12).forEach(function (recipe) {
+        var limitedDescription =
+          recipe.description.slice(0, 50) +
+          (recipe.description.length > 75 ? "..." : "");
+        var featuredRecipesHtml = `
+                                    <div class="col-md-6 col-lg-4 col-xl-3 mt-3" >
+                                        <div class="rounded position-relative fruite-item">
+                                            <div class="fruite-img">
+                                                <img src="${recipe.image}"
+                                                    class="img-fluid w-100 rounded-top" alt="${recipe.title}" style="height: 200px;">
+                                            </div>
+                                      
+                                            <div class="p-4 border border-secondary border-top-0 rounded-bottom">
+                                                <h4>${recipe.title}</h4>
+                                                <p>${limitedDescription}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                `;
+        $("#featuredRecipeContainer").append(featuredRecipesHtml);
+      });
+    });
+  }
+
+  function getFeaturedUsers() {
+    $.get("users.json", function (data) {
+        data.sort(() => Math.random() - 0.5);
+        data.slice(0, 6).forEach(function (user) {
+            var featuredUsersHtml = `
+                                     <div class="col-lg-6 col-xl-4">
+                                        <div class="p-4 rounded bg-light">
+                                            <div class="row align-items-center">
+                                                <div class="col-6">
+                                                    <img src="${user.avatar}" class="img-fluid rounded-circle w-100" alt="">
+                                                </div>
+                                                <div class="col-6">
+                                                    <a href="#" class="h5">${user.username}</a>
+                                                    <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i
+                                                            class="fa fa-star me-2 text-primary"></i>Follow</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                `;
+        $("#featuredUsersContainer").append(featuredUsersHtml);
+      });
+    });
+  }
+
   // Call the function to fetch and display recipe data
   getRecipes();
   getRecipeDetails();
+  getFeaturedRecipes();
+  getFeaturedUsers();
 });
