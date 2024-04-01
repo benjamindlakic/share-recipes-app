9f56220379fd92a98be422bf8ffe03f81618e717
benjamindlakic <benjomaster7@gmail.com>
2024-04-01 21:12:27 +0200

    more work on ajax and form validation

--- assets/js/form.js
+++ assets/js/form.js
@@ -40,9 +40,178 @@ $("#register-form").validate({
     $("#register-form")[0].reset();
     console.log(users); 
     unblockUi("#register-form"); 
+    window.location.href = "#login";
+
   },
 });
 
+$("#login-form").validate({
+  rules: {
+    username: {
+      required: true,
+      minlength: 5,
+    },
+    password: {
+      required: true,
+      minlength: 5,
+    },
+  },
+  messages: {
+    username: {
+      required: "You have to fill it in!",
+      minlength: "Too short buddy!",
+    },
+  },
+  submitHandler: function (form, event) {
+    event.preventDefault();
+    blockUi("#login-form");
+    let data = serializeForm(form);
+    console.log("Login data:", data);
+    unblockUi("#login-form");
+    window.location.href = "#home";
+  },
+});
+
+$("#edit-profile-form").validate({
+  rules: {
+    first_name: {
+      required: true,
+      minlength: 5,
+    },
+    bio: {
+      required: true,
+      maxlength: 500,
+    },
+  },
+  messages: {
+    first_name: {
+      required: "You have to fill it in!",
+      minlength: "Too short buddy!",
+    },
+  },
+  submitHandler: function (form, event) {
+    event.preventDefault();
+    blockUi("#edit-profile-form");
+    let data = serializeForm(form);
+    // Update the user's profile data
+    // You can use the user's ID to find and update the corresponding user in the 'users' array
+    // Example: users[userId] = data;
+    console.log("Updated profile data:", data);
+    unblockUi("#edit-profile-form");
+    // Redirect to the profile page or any other desired location
+    // Example: window.location.href = "#profile";
+    // alert("Profile has been successfully updated");
+  },
+});
+$("#upload-recipe-form").validate({
+  rules: {
+    title: {
+      required: true,
+      minlength: 5,
+    },
+    description: {
+      required: true,
+      minlength: 10,
+    },
+    ingredients: {
+      required: true,
+      minlength: 10,
+    },
+    instructions: {
+      required: true,
+      minlength: 10,
+    },
+    image: {
+      required: true,
+      accept: "image/*",
+    },
+    preptime: {
+      required: true,
+      minlength: 1,
+    },
+  },
+  messages: {
+    title: {
+      required: "You have to fill it in!",
+      minlength: "Too short buddy!",
+    },
+    description: {
+      required: "You have to fill it in!",
+      minlength: "Too short buddy!",
+    },
+    ingredients: {
+      required: "You have to fill it in!",
+      minlength: "Too short buddy!",
+    },
+    instructions: {
+      required: "You have to fill it in!",
+      minlength: "Too short buddy!",
+    },
+    image: {
+      required: "You have to select an image!",
+      accept: "Only image files are allowed!",
+    },
+    preptime: {
+      required: "You have to fill it in!",
+      minlength: "Too short buddy!",
+    },
+  },
+  submitHandler: function (form, event) {
+    event.preventDefault();
+    blockUi("#upload-recipe-form");
+    let data = serializeForm(form);
+    // Perform the upload operation
+    // Example: $.ajax({ url: "/upload", type: "POST", data: data, processData: false, contentType: false, success: function(response) { console.log(response); }, error: function(error) { console.log(error); } });
+    console.log("Uploaded recipe:", data);
+    unblockUi("#upload-recipe-form");
+    // Redirect to the recipe page or any other desired location
+    alert("Recipe has been successfully uploaded");
+  },
+});
+$("#change-password-form").validate({
+  rules: {
+    current_password: {
+      required: true,
+      minlength: 5,
+    },
+    new_password: {
+      required: true,
+      minlength: 5,
+    },
+    confirm_password: {
+      required: true,
+      minlength: 5,
+      //equalTo: "#new_password",
+    },
+  },
+  messages: {
+    current_password: {
+      required: "You have to fill it in!",
+      minlength: "Too short buddy!",
+    },
+    new_password: {
+      required: "You have to fill it in!",
+      minlength: "Too short buddy!",
+    },
+    confirm_password: {
+      required: "You have to fill it in!",
+      minlength: "Too short buddy!",
+      //equalTo: "The new password and confirm password fields should be the same",
+    },
+  },
+  submitHandler: function (form, event) {
+    event.preventDefault();
+    blockUi("#change-password-form");
+    let data = serializeForm(form);
+    // Update the user's password
+    // You can use the user's ID to find and update the corresponding user in the 'users' array
+    // Example: users[userId].password = data.new_password;
+    console.log("Changed password:", data);
+    unblockUi("#change-password-form");
+    // Redirect to the profile page or any other desired location
+    alert("Password has been successfully changed");
+  },
+});
 // function objectFormHandler(form, event) {
 //   // TODO check whether the form action is create a new user or update a existing user
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -36,86 +36,121 @@
 })(jQuery);
 
 $(document).ready(function () {
-  $(document).on("click", ".btn-like", function () {
-    $(this).toggleClass("liked");
-    var icon = $(this).find("i");
-    icon.toggleClass("bi-heart bi-heart-fill");
-  });
+      // Variable to store recipe data
+  var recipeData;
+  var usersData;
 
-  // Function to fetch and display recipe data
+  // Function to fetch and store recipe data
   function getRecipes() {
     $.get("recipes.json", function (data) {
-      data.forEach(function (recipe) {
-        var limitedDescription =
-          recipe.description.slice(0, 150) +
-          (recipe.description.length > 200 ? "..." : "");
-        var recipefeedHtml = `
-                        <div class="card">
-                            <a href=""><img src="${recipe.image}" class="card-img-top" alt="${recipe.title}">
-                            <div class="card-body">
-                                <h5 class="card-title">${recipe.title}</h5>
-                            </a>
-                                <p class="card-text">${limitedDescription}</p>
-                                <div class="social-buttons">
-                                    <button class="btn btn-like"><i class="bi bi-heart"></i> Like</button>
-                                    <button class="btn btn-comment"><i class="bi bi-chat"></i> Comment</button>
-                                    <button class="btn btn-share"><i class="bi bi-share"></i> Share</button>
-                                </div>
-                            </div>
-                        </div>
-                        `;
-        $("#feedContainer").append(recipefeedHtml);
-      });
+      recipeData = data;
+      displayRecipes(recipeData);
     });
   }
-  // Function to fetch and display recipe data
-  function getRecipeDetails() {
-    $.get("recipes.json", function (data) {
-      var recipe = data[0]; 
-      var recipeHtml = `
-                    <div class="row my-5">
-                        <div class="col-lg-6">
-                            <div class="rounded">
-                                <img src="${recipe.image}" class="img-fluid rounded mx-auto d-block" alt="${recipe.title}" style="width: 60%; height: auto;">
-                            </div>
-                        </div>
-                        <div class="col-lg-6 mt-3">
-                            <div>
-                                <h4 class="fw-bold mb-3">${recipe.title}</h4>
-                                <p class="mb-4">${recipe.description}</p>
-                                <a href="#" class="btn btn-primary rounded-pill px-4 mb-2"><i class="fa fa-heart me-2"></i>Like</a>
-                            </div>
-                    </div>
-                    <hr class="mt-5">
-                    <div class="row mt-3">
-                            <div class="col-lg-6 my-3">
-                                <h5 class="fw-bold mb-3">Ingredients:</h5>
-                                <ul class="list-group">
-                                    ${recipe.ingredients
-                                      .map(
-                                        (ingredient) =>
-                                          `<li class="list-group-item">${ingredient}</li>`
-                                      )
-                                      .join("")}
-                                </ul>
-                            </div>
-                            <div class="col-lg-6 mt-3">
-                                <h5 class="fw-bold mb-3">Instructions:</h5>
-                                <ol class="list-group">
-                                    ${recipe.instructions
-                                      .map(
-                                        (instruction) =>
-                                          `<li class="list-group-item">${instruction}</li>`
-                                      )
-                                      .join("")}
-                                </ol>
-                            </div>
-                        </div>
-                    `;
-      $("#recipeContainer").append(recipeHtml);
+  function getUsers() {
+    $.get("users.json", function (data) {
+      usersData = data;
+      displayUsers(usersData);
     });
   }
 
+  // Function to display recipe cards
+  function displayRecipes(data) {
+    data.forEach(function (recipe) {
+      var limitedDescription =
+        recipe.description.slice(0, 150) +
+        (recipe.description.length > 200 ? "..." : "");
+      var recipefeedHtml = `
+                    <div class="card" data-id="${recipe.id}">
+                        <img src="${recipe.image}" class="card-img-top" alt="${recipe.title}">
+                        <div class="card-body">
+                            <h5 class="card-title">${recipe.title}</h5>
+                            <p class="card-text">${limitedDescription}</p>
+                            <div class="social-buttons">
+                                <button class="btn btn-like"><i class="bi bi-heart"></i> Like</button>
+                                <button class="btn btn-comment"><i class="bi bi-chat"></i> Comment</button>
+                                <button class="btn btn-share"><i class="bi bi-share"></i> Share</button>
+                            </div>
+                        </div>
+                    </div>
+                    `;
+      $("#feedContainer").append(recipefeedHtml);
+    });
+  }
+
+  // Event delegation for recipe cards
+  $("#feedContainer").on("click", ".card", function () {
+    var recipeId = $(this).data("id");
+    window.location.href = "#recipe-detail";
+    getRecipeDetails(recipeId);
+  });
+
+  // Function to display recipe details
+function getRecipeDetails(recipeId) {
+  var recipe = recipeData.find((recipe) => recipe.id == recipeId);
+  if (recipe) {
+      $("#recipeContainer").empty();
+      var recipeHtml = `
+          <div class="row my-5">
+              <div class="col-lg-6">
+                  <div class="rounded">
+                      <img src="${recipe.image}" class="img-fluid rounded mx-auto d-block" alt="${recipe.title}" style="width: 60%; height: auto;">
+                  </div>
+              </div>
+              <div class="col-lg-6 mt-3">
+                  <div>
+                      <h4 class="fw-bold mb-3">${recipe.title}</h4>
+                      <p class="mb-4">${recipe.description}</p>
+                      <a class="btn btn-primary rounded-pill px-4 mb-2 like-btn" style="background-color:white; color: black;"><i class="fa fa-heart me-2"></i><span class="like-text">Like</span></a>
+                  </div>
+              </div>
+              <hr class="mt-5">
+              <div class="row mt-3">
+                  <div class="col-lg-6 my-3">
+                      <h5 class="fw-bold mb-3">Ingredients:</h5>
+                      <ul class="list-group">
+                          ${recipe.ingredients
+                              .map(
+                                  (ingredient) =>
+                                  `<li class="list-group-item">${ingredient}</li>`
+                              )
+                              .join("")}
+                      </ul>
+                  </div>
+                  <div class="col-lg-6 mt-3">
+                      <h5 class="fw-bold mb-3">Instructions:</h5>
+                      <ol class="list-group">
+                          ${recipe.instructions
+                              .map(
+                                  (instruction) =>
+                                  `<li class="list-group-item">${instruction}</li>`
+                              )
+                              .join("")}
+                      </ol>
+                  </div>
+              </div>
+          </div>`;
+
+      $("#recipeContainer").append(recipeHtml);
+
+      // Event delegation to handle Like button click
+      $("#recipeContainer").on("click", ".like-btn", function() {
+          var $likeBtn = $(this);
+          $likeBtn.toggleClass("liked");
+          if ($likeBtn.hasClass("liked")) {
+          $likeBtn.find('.like-text').text("Unlike");
+          $likeBtn.css("background-color", "green");
+          $likeBtn.css("color", "white");
+
+          } else {
+          $likeBtn.find('.like-text').text("Like");
+          $likeBtn.css("background-color", "white");
+          $likeBtn.css("color", "black");
+          }
+      });
+  }
+}
+  
   function getFeaturedRecipes() {
     $.get("recipes.json", function (data) {
       data.sort(() => Math.random() - 0.5);
@@ -125,7 +160,7 @@ $(document).ready(function () {
           (recipe.description.length > 75 ? "..." : "");
         var featuredRecipesHtml = `
                                     <div class="col-md-6 col-lg-4 col-xl-3 mt-3" >
-                                        <div class="rounded position-relative fruite-item">
+                                        <div class="rounded position-relative fruite-item" data-id="${recipe.id}">
                                             <div class="fruite-img">
                                                 <img src="${recipe.image}"
                                                     class="img-fluid w-100 rounded-top" alt="${recipe.title}" style="height: 200px;">
@@ -144,41 +179,76 @@ $(document).ready(function () {
     });
   }
 
-  function getFeaturedUsers() {
-    $.get("users.json", function (data) {
-        data.sort(() => Math.random() - 0.5);
-        data.slice(0, 6).forEach(function (user) {
-            var featuredUsersHtml = `
-                                     <div class="col-lg-6 col-xl-4 mt-3">
-                                        <div class="p-4 rounded bg-light">
-                                            <div class="row align-items-center">
-                                                <div class="col-6">
-                                                    <img src="${user.avatar}" class="img-fluid rounded-circle w-100" alt="">
-                                                </div>
-                                                <div class="col-6">
-                                                    <a href="#" class="h5">${user.username}</a>
-                                                    <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i
-                                                            class="fa fa-star me-2 text-primary"></i>Follow</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+// Function to display recipe cards
+function displayUsers(data) {
+  data.sort(() => Math.random() - 0.5);
+  data.slice(0, 6).forEach(function (user) {
+      var featuredUsersHtml = `
+        <div class="col-lg-6 col-xl-4 mt-3">
+          <div class="p-4 rounded bg-light">
+            <div class="row align-items-center">
+              <div class="col-6">
+                <img src="${user.avatar}" class="img-fluid rounded-circle w-100" alt="">
+              </div>
+              <div class="col-6">
+                <a href="#" class="h5">${user.username}</a>
+                <p class="mb-0">Followers: ${user.followers}</p>
+                <a class="btn border border-secondary rounded-pill px-3 text-primary follow-btn">${user.following ? 'Following' : 'Follow'}</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
 
-                `;
-        $("#featuredUsersContainer").append(featuredUsersHtml);
+      $("#featuredUsersContainer").append(featuredUsersHtml);
+  });
+
+  // Event delegation to handle button clicks
+  $("#featuredUsersContainer").on("click", ".follow-btn", function() {
+      var $button = $(this);
+      if ($button.text() === "Following") {
+          $button.text("Follow");
+          $button.css("background-color", "");
+      } else {
+          $button.text("Following");
+          $button.css("background-color", "green");
+      }
+  });
+}
+
+
+  // Event delegation for featured recipe cards
+  $("#featuredRecipeContainer").on("click", ".fruite-item", function () {
+    var recipeId = $(this).data("id");
+    window.location.href = "#recipe-detail";
+    getRecipeDetails(recipeId);
+  });
+
+  // function getFeaturedUsers() {
+  //   $.get("users.json", function (data) {
+        
+  //   });
+  // }
+  // Function to get user details
+  function getUserDetails(userId) {
+    $.get("users.json", function (data) {
+      var user = data.find(function (user) {
+        return user.id === userId;
       });
+      if (user) {
+        var userDetailsHtml = `
+              <img src="${user.avatar}" alt="Profile Picture" class="img-fluid rounded-circle" style="width: 150px; height: 150px;">
+              <h2 class="mt-3">${user.name}</h2>
+              <p>Followers: ${user.followers}</p>
+        `;
+        $("#userDetailsContainer").html(userDetailsHtml);
+      }
     });
   }
-
-  // Call the function to fetch and display recipe data
+  
   getRecipes();
-  getRecipeDetails();
+  getUsers();
   getFeaturedRecipes();
-  getFeaturedUsers();
-  // Function to handle recipe click event
-  $(document).on("click", ".card-img", function () {
-    var recipeId = $(this).data("id");
-    // Redirect to recipe details page with recipeId as parameter
-    window.location.href = "recipe-details.html?id=" + recipeId;
-});
+  getUserDetails(2);
+
 });
